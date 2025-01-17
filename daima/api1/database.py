7591d33750e11ba84155b0de19736e973eff097f
xwzdr <344768741@qq.com>
2025-01-17 22:02:55 +0300

    database.py

--- daima/api1/database.py
+++ daima/api1/database.py
@@ -3,32 +3,32 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
 
-# 加载环境变量
+# Loading environment variables
 load_dotenv()
 
-# 从环境变量中获取数据库配置
+# Getting the database configuration from environment variables
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-# 打印调试信息
+# Print debugging information
 print(f"DB_HOST: {DB_HOST}, DB_PORT: {DB_PORT}, DB_NAME: {DB_NAME}, DB_USER: {DB_USER}, DB_PASSWORD: {DB_PASSWORD}")
 
-# 构建数据库 URL
+# Building Database URLs
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# 创建数据库引擎
+# Creating a Database Engine
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# 获取数据库会话的生成器
+# Get the database session generator
 def get_db():
     db = SessionLocal()
     try:
-        yield db  # 使用 yield 使其成为生成器
+        yield db  # Use yield to make it a generator
     finally:
-        db.close()  # 确保会话在完成后关闭
+        db.close()  # Ensure that the session is closed after completion
